03c45327dd7d65e22685994137900561803b99fd
akhkim <166858130+akhkim@users.noreply.github.com>
2024-07-29 00:14:29 -0400

    Add files via upload

--- src/javascript/World/Controls.js
+++ src/javascript/World/Controls.js
@@ -366,7 +366,7 @@ export default class Controls extends EventEmitter
 
                 this.touch.boost.touchIdentifier = touch.identifier
 
-                this.actions.down = true
+                this.actions.up = true
                 this.actions.boost = true
 
                 this.touch.boost.$border.style.opacity = '0.5'
@@ -382,7 +382,7 @@ export default class Controls extends EventEmitter
 
             if(touch)
             {
-                this.actions.down = false
+                this.actions.up = false
                 this.actions.boost = false
 
                 this.touch.boost.$border.style.opacity = '0.25'
@@ -450,7 +450,7 @@ export default class Controls extends EventEmitter
 
                 this.touch.forward.touchIdentifier = touch.identifier
 
-                this.actions.down = true
+                this.actions.up = true
 
                 this.touch.forward.$border.style.opacity = '0.5'
 
@@ -465,7 +465,7 @@ export default class Controls extends EventEmitter
 
             if(touch)
             {
-                this.actions.down = false
+                this.actions.up = false
 
                 this.touch.forward.$border.style.opacity = '0.25'
 
@@ -614,7 +614,7 @@ export default class Controls extends EventEmitter
 
                 this.touch.backward.touchIdentifier = touch.identifier
 
-                this.actions.up = true
+                this.actions.down = true
 
                 this.touch.backward.$border.style.opacity = '0.5'
 
@@ -629,7 +629,7 @@ export default class Controls extends EventEmitter
 
             if(touch)
             {
-                this.actions.up = false
+                this.actions.down = false
 
                 this.touch.backward.$border.style.opacity = '0.25'
 
