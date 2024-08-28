dc46e95946d484252383f29342e6466f41d23e9b
akhkim <166858130+akhkim@users.noreply.github.com>
2024-08-28 18:58:52 -0400

    Add files via upload

--- src/javascript/World/Physics.js
+++ src/javascript/World/Physics.js
@@ -454,60 +454,35 @@ export default class Physics
             /**
              * Steering
              */
-            if(this.controls.touch)
+            const steerStrength = this.time.delta * this.car.options.controlsSteeringSpeed
+
+            // Steer right
+            if(this.controls.actions.right)
             {
-                let deltaAngle = 0
-
-                if(this.controls.touch.joystick.active)
+                this.car.steering += steerStrength
+            }
+            // Steer left
+            else if(this.controls.actions.left)
+            {
+                this.car.steering -= steerStrength
+            }
+            // Steer center
+            else
+            {
+                if(Math.abs(this.car.steering) > steerStrength)
                 {
-                    // Calculate delta between joystick and car angles
-                    deltaAngle = (this.controls.touch.joystick.angle.value - this.car.angle + Math.PI) % (Math.PI * 2) - Math.PI
-                    deltaAngle = deltaAngle < - Math.PI ? deltaAngle + Math.PI * 2 : deltaAngle
+                    this.car.steering -= steerStrength * Math.sign(this.car.steering)
                 }
-
-                // Update steering directly
-                const goingForward = Math.abs(this.car.forwardSpeed) < 0.01 ? true : this.car.goingForward
-                this.car.steering = deltaAngle * (goingForward ? - 1 : 1)
-
-                // Clamp steer
-                if(Math.abs(this.car.steering) > this.car.options.controlsSteeringMax)
+                else
                 {
-                    this.car.steering = Math.sign(this.car.steering) * this.car.options.controlsSteeringMax
+                    this.car.steering = 0
                 }
             }
 
-            if(!this.controls.touch || !this.controls.touch.joystick.active)
+            // Clamp steer
+            if(Math.abs(this.car.steering) > this.car.options.controlsSteeringMax)
             {
-                const steerStrength = this.time.delta * this.car.options.controlsSteeringSpeed
-
-                // Steer right
-                if(this.controls.actions.right)
-                {
-                    this.car.steering += steerStrength
-                }
-                // Steer left
-                else if(this.controls.actions.left)
-                {
-                    this.car.steering -= steerStrength
-                }
-                // Steer center
-                else
-                {
-                    if(Math.abs(this.car.steering) > steerStrength)
-                    {
-                        this.car.steering -= steerStrength * Math.sign(this.car.steering)
-                    }
-                    else
-                    {
-                        this.car.steering = 0
-                    }
-                }
-
-                // Clamp steer
-                if(Math.abs(this.car.steering) > this.car.options.controlsSteeringMax)
-                {
-                    this.car.steering = Math.sign(this.car.steering) * this.car.options.controlsSteeringMax
-                }
+                this.car.steering = Math.sign(this.car.steering) * this.car.options.controlsSteeringMax
             }
             
             // Update wheels
