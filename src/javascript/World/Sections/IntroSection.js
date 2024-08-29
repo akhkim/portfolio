33a18b21c642e469878afaa9edfb8997b8e2df84
akhkim <166858130+akhkim@users.noreply.github.com>
2024-08-29 00:47:39 -0400

    Add files via upload

--- src/javascript/World/Sections/IntroSection.js
+++ src/javascript/World/Sections/IntroSection.js
@@ -244,7 +244,7 @@ export default class IntroSection
         this.objects.add({
             base: this.resources.items.introDevBase.scene,
             collision: this.resources.items.introDevCollision.scene,
-            offset: new THREE.Vector3(0, 17.5, 0.25),
+            offset: new THREE.Vector3(0.5, 17.5, 0.25),
             rotation: new THREE.Euler(0, 0, 0),
             shadow: { sizeX: 2.25, sizeY: 1.5, offsetZ: - 0.6, alpha: 0.3 },
             mass: 1,
