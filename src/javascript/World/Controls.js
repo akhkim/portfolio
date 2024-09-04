67f9b58ec454b401ddf195f1ec2d928e9abf1bf2
akhkim <166858130+akhkim@users.noreply.github.com>
2024-09-04 15:56:24 -0400

    Update Controls.js

--- src/javascript/World/Controls.js
+++ src/javascript/World/Controls.js
@@ -356,7 +356,7 @@ export default class Controls extends EventEmitter
 
                 this.touch.boost.touchIdentifier = touch.identifier
 
-                this.actions.up = true
+                this.actions.down = true
                 this.actions.boost = true
 
                 this.touch.boost.$border.style.opacity = '0.5'
@@ -372,7 +372,7 @@ export default class Controls extends EventEmitter
 
             if(touch)
             {
-                this.actions.up = false
+                this.actions.down = false
                 this.actions.boost = false
 
                 this.touch.boost.$border.style.opacity = '0.25'
